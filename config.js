e260f88496b44defa7b1c934785ce8fdd0ef87d1
Arjun <arjun4180@gmail.com>
2022-06-07 21:35:25 +0530

    Fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "68e8410ea5bda4695134f9f86d2f8959"
+        SECRET_KEY: "38c3e1a094f8a1525de92d2dbd056ec5"
         
     }
 }
